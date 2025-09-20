ade53b2998cc3a02d3cab8499f090efe6efc6001
bourbonut <ben.bourbon06@gmail.com>
2025-09-20 20:14:06 +0200

    Update `pointer` function

--- detroit_live/events/pointer.py
+++ detroit_live/events/pointer.py
@@ -1,6 +1,7 @@
-from .types import MouseEvent
+from .types import MouseEvent, WheelEvent
 
-def pointer(event: MouseEvent) -> tuple[float, float]:
-    if event.rect_top is None or event.rect_left is None:
+def pointer(event: MouseEvent | WheelEvent) -> tuple[float, float]:
+    if isinstance(event, MouseEvent):
         return event.page_x, event.page_y
-    return event.client_x - event.rect_left, event.client_y - event.rect_top
+    else:
+        return event.client_x - event.rect_left, event.client_y - event.rect_top
