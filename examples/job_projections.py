f07f96b5cde21eb955c4b08c8f9f64322956099b
bourbonut <ben.bourbon06@gmail.com>
2025-09-28 11:56:00 +0200

    Update `examples/job_projections.py` and add `examples/job_projection.css`

--- examples/job_projections.py
+++ examples/job_projections.py
@@ -11,10 +11,11 @@ import requests
 URL = "https://gist.githubusercontent.com/eidietrich/0047db2bfcfae1543ff37c70474587d3/raw/51bcb25225d5517c40fc8328645973183ed140e6/trimmed-for-vis.json"
 Margin = namedtuple("Margin", ("top", "right", "bottom", "left"))
 
+PWD = Path(__file__).resolve().parent
 
+# Download data if not found else load data from `examples/data` folder
 def load_data() -> pl.DataFrame:
-    pwd = Path(__file__).resolve().parent
-    data_path = pwd / "data"
+    data_path = PWD / "data"
     if data_path.exists():
         path = data_path / "trimmed-for-vis.csv"
         if path.exists():
@@ -25,6 +26,7 @@ def load_data() -> pl.DataFrame:
     return df
 
 
+# Prepare data processing
 color_domain = [
     "Natural Resources",
     "Construction",
@@ -76,6 +78,7 @@ clean_ed_level = {
 ed_order = {value: i for i, (key, value) in enumerate(clean_ed_level.items())}
 ed_order["High school diploma or less"] = 0
 
+# Data processing
 df: pl.DataFrame = (
     load_data()
     .with_columns(
@@ -94,72 +97,83 @@ df: pl.DataFrame = (
     .filter((pl.col("Median Wage 2018") > 0) & (pl.col("Median Wage 2018") <= 140_000))
 )
 
-width = 600
+# Declare the chart dimensions and margins.
+legend_width = 150
+width = 600 + legend_width
 height = 300
-margin = Margin(20, 30, 30, 40)
+margin = Margin(10, 15 + legend_width, 40, 55)
 
+# Declare all scales.
+# `x` for horizontal position
+# `y` for horizontal position
+# `radius` for circle radius
+# `color` for circle fill color
 x = d3.scale_linear(
     [0, df["turnover"].max()], [margin.left, width - margin.right]
 ).nice()
 y = d3.scale_linear([0, 140_000], [height - margin.bottom, margin.top]).nice()
 
-radius = d3.scale_linear([df["openings"].min(), df["openings"].max()], [3, 20])
+radius = d3.scale_sqrt([df["openings"].min(), df["openings"].max()], [2, 20])
 color = d3.scale_ordinal(color_domain, color_range)
 
+# Initialize the HTML document, add style in `<head>`.
 html = d3live.create("html")
-style = """
-.tooltip {
-    position: absolute;
-    display: block;
-    margin: 0;
-    font-size: 0.875rem;
-    font-family: -apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol";
-    font-weight: 400;
-    word-wrap: break-word;
-    background-color: white;
-    border: 1px solid lightgray;
-    border-radius: 4px;
-    padding-block: 25px;
-    padding-inline: 10px;
-    box-shadow: 1px 2px 2px lightgray;
-}
-
-.grid {
-    display: grid;
-    grid-template-columns:auto auto;
-    gap: 0px 5px;
-}
-
-.category {
-    text-align: end;
-    color: grey;
-}
-"""
-(html.append("head").append("style").text(style))
+html.append("head").append("style").text((PWD / "job_projections.css").read_text())
 body = html.append("body")
 
+# scale factor; 2 to get two times bigger chart.
+k = 2
+
+# Declare the SVG container.
 svg = (
     body.append("div")
     .append("svg")
-    .attr("width", width)
-    .attr("height", height)
+    .attr("width", width * k)
+    .attr("height", height * k)
     .attr("viewBox", [0, 0, width, height])
 )
+
+# Add y-axis with label
+tx = sum(x.get_range()) // 2
+ty = 3 * margin.bottom // 4
 
 (
     svg.append("g")
     .attr("transform", f"translate(0,{height - margin.bottom})")
     .call(d3.axis_bottom(x).set_ticks(None, "%"))
-    .call(lambda g: g.select(".domain").remove())
+    .call(
+        lambda g: (
+            g.append("g")
+            .attr("class", "label")
+            .attr("transform", "translate(0.5, 0)")
+            .append("text")
+            .attr("transform", f"translate({tx}, {ty})")
+            .text("Occupation annual turnover rate")
+        )
+    )
 )
+
+# Add y-axis with label
+tx = -sum(y.get_range()) // 2
+ty = -3 * margin.left // 4
 
 (
     svg.append("g")
     .attr("transform", f"translate({margin.left},0)")
     .call(d3.axis_left(y).set_ticks(width / 80, "$,~s"))
-    .call(lambda g: g.select(".domain").remove())
+    .call(
+        lambda g: (
+            g.append("g")
+            .attr("class", "label")
+            .attr("transform", "matrix(0 -1 1 0 0.5 0)")
+            .append("text")
+            .attr("transform", f"translate({tx}, {ty})")
+            .text("Median wage, 2018")
+        )
+    )
 )
 
+# Add grid
 svg.append("g").call(
     lambda g: (
         g.attr("stroke", "currentColor")
@@ -191,6 +205,7 @@ svg.append("g").call(
     )
 )
 
+# Add circles with specific radius and fill color for each circle
 circles = (
     svg.append("g")
     .attr("fill-opacity", 0.5)
@@ -206,6 +221,7 @@ circles = (
     .attr("stroke", lambda d: color(d["sector_cat"]))
 )
 
+# Add rule for `yRef`.
 line = (
     svg.append("g")
     .append("line")
@@ -213,23 +229,116 @@ line = (
     .attr("x2", x.get_range()[1])
     .attr("y1", y(33900))
     .attr("y2", y(33900))
-    .attr("stroke", "grey")
+    .attr("stroke", "#666")
     .attr("stroke-width", 1.5)
     .attr("stroke-opacity", 0.75)
     .attr("fill", "none")
     .style("user-select", "none")
 )
 
-tooltip = body.append("div")
+# Add legend container to the SVG container.
+legend = (
+    svg.append("g")
+    .attr("class", "legend")
+    .attr("transform", f"translate({width - legend_width}, 0)")
+)
 
-(tooltip.style("opacity", 0).attr("class", "tooltip"))
+# Openings legend container.
+openings_legend = legend.append("g").attr("transform", f"translate(0, {margin.top})")
+
+# Openings legend title.
+(
+    openings_legend.append("text")
+    .style("dominant-baseline", "text-before-edge")
+    .style("font-weight", "bold")
+    .text("Openings projected")
+)
+
+# Range of openings values.
+data_openings = [10, 100, 500, 1_000, 2_000]
+openings_enter = openings_legend.select_all("circle").data(data_openings).enter()
+
+# Recursive function which returns the y-coordinate translation value for each
+# opening's circle.
+def dy(d, i):
+    if i == 0:
+        return 20 + radius(d)
+    else:
+        previous = data_openings[i - 1]
+        return radius(previous) + 5 + radius(d) + dy(previous, i - 1)
 
 
+# Add opening's circles.
+rmax = radius(2000)
+openings_enter.append("circle").attr("cx", rmax).attr("cy", dy).attr("r", radius)
+
+# Add opening's values (from `data_openings`) as text.
+(
+    openings_enter.append("text")
+    .attr("transform", lambda d, i: f"translate({2 * rmax + 5}, {dy(d, i) + 3})")
+    .text(d3.format(","))
+)
+
+# Computes the last y-coordinate translation value as the starting y-coordinate
+# for occupation legend.
+ty_max = dy(2000, len(data_openings) - 1) + 3
+
+# Occupation legend container.
+occupation_legend = legend.append("g").attr(
+    "transform", f"translate(0, {margin.top + ty_max + rmax + 5})"
+)
+
+# Occupation legend title.
+(
+    occupation_legend.append("text")
+    .style("dominant-baseline", "text-before-edge")
+    .style("font-weight", "bold")
+    .text("Occupation sector")
+)
+
+# Radius of occupation's circles
+rcircle = 5
+
+
+# Recursive function which returns the y-coordinate translation value for each
+# occupation's circle.
+def dy(i):
+    if i == 0:
+        return 15 + rcircle
+    else:
+        return 5 + 2 * rcircle + dy(i - 1)
+
+
+# Prepare occupation elements
+occupation_enter = occupation_legend.select_all("circle").data(color_domain).enter()
+
+# Add occupation's circles.
+(
+    occupation_enter.append("circle")
+    .attr("cx", rcircle)
+    .attr("cy", lambda _, i: dy(i))
+    .attr("r", rcircle)
+    .attr("fill", color)
+)
+
+# Add occupation's values (from `color_domain`) as text.
+(
+    occupation_enter.append("text")
+    .attr("transform", lambda _, i: f"translate({2 * rcircle + 5}, {dy(i) + 3})")
+    .text(lambda d: d)
+)
+
+# Add tooltop after the SVG container
+tooltip = body.append("div").style("opacity", 0).attr("class", "tooltip")
+
+
+# Mouseover event function
 def mouseover(event, d, node):
     tooltip.style("opacity", 0.95)
     d3.select(node).style("stroke", "#222")
 
 
+# Mousemove event function
 def mousemove(event, d, node):
     point = d3live.pointer(event)
     local_data = [
@@ -244,8 +353,8 @@ def mousemove(event, d, node):
     ]
     tooltip.select_all().remove()
     (
-        tooltip.style("left", f"{point[0] + 30}px")
-        .style("top", f"{point[1] + 30}px")
+        tooltip.style("left", f"{point[0] * k + 30}px")
+        .style("top", f"{point[1] * k + 30}px")
         .append("div")
         .attr("class", "grid")
         .select_all()
@@ -257,14 +366,17 @@ def mousemove(event, d, node):
     )
 
 
+# Mouseleave event function
 def mouseleave(event, d, node):
-    tooltip.style("opacity", 0).style("left", f"{width + 30}px").style(
-        "top", f"{height + 30}px"
+    (
+        tooltip.style("opacity", 0)
+        .style("left", f"{-width * k + 30}px")
+        .style("top", f"{-height * k + 30}px")
     )
 
     d3.select(node).style("stroke", color(d["sector_cat"]))
 
-
+# Add events on circles
 (
     circles.on(
         "mouseover",
